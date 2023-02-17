ede8b703a4482afe0f5470f1f8ea55310fa1b4ad
Albedo <duc.nghiem@blockodyssey.io>
2023-02-17 17:47:50 +0700

    middleware

--- project/server/src/shared/middleware.ts
+++ project/server/src/shared/middleware.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
+import { JWT_SECRET } from './config'
+import { verify } from 'jsonwebtoken'
 
-export async function userAuthorization(req: Request, res: Response, next: NextFunction): Promise<> {
+export async function userAuthorization(req: Request, res: Response, next: NextFunction) {
     try {
-        
+        const token = req.headers.authorization?.split(' ')[1]
+        if (token) {
+            verify(token, JWT_SECRET)
+            next()
+            return
+        }
+        throw { code: 401, message: "Unauthorized" }
     } catch (e) {
-        return { code: 401 }
+        throw { code: 401, message: "Unauthorized" }
     }    
 }
--- project/server/src/shared/utils.ts
+++ project/server/src/shared/utils.ts
@@ -3,3 +3,7 @@ export function validatePaginationParams(page: number, pageSize: number) {
   if (pageSize <= 0) throw new Error('pageSize should be greater than 0')
   if (pageSize >= 1000) throw new Error('pageSize should be lower than 1000')
 }
+
+export function parseHeaderToken() {
+  
+}
